9ec6afe8fb5981d3b954f72fab8f4864c1ca9b9b
Eduardo Santos <edu.makermakers@gmail.com>
2025-02-17 21:44:47 -0300

    init: aula 1

---
+++ src/routes/subscribe-to-event-route.ts
@@ -0,0 +1,32 @@
+import type { FastifyPluginAsyncZod } from "fastify-type-provider-zod";
+import { z } from "zod";
+
+export const subscribeToEventRoute: FastifyPluginAsyncZod = async (app) => {
+  app.post(
+    "/subscriptions",
+    {
+      schema: {
+        summary: "Subscribe someone to the event",
+        tags: ["Subscriptions"],
+        description: "Subscribe someone to the event",
+        body: z.object({
+          name: z.string(),
+          email: z.string().email(),
+        }),
+        response: {
+          201: z.object({
+            name: z.string(),
+            email: z.string().email(),
+          }),
+        },
+      },
+    },
+    async (request, reply) => {
+      const { name, email } = request.body;
+
+      // Criação da inscrição no BD
+
+      return reply.status(201).send({ name, email });
+    }
+  );
+};
---
+++ src/server.ts
@@ -0,0 +1,46 @@
+import { fastifyCors } from "@fastify/cors";
+import { fastifySwagger } from "@fastify/swagger";
+import { fastifySwaggerUi } from "@fastify/swagger-ui";
+// . Libs
+import { fastify } from "fastify";
+import {
+  type ZodTypeProvider,
+  jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler,
+} from "fastify-type-provider-zod";
+
+// . Variaveis de ambiente
+import { env } from "./env";
+
+// . Rotas
+import { subscribeToEventRoute } from "./routes/subscribe-to-event-route";
+
+// . Configuração do servidor
+const app = fastify().withTypeProvider<ZodTypeProvider>();
+
+app.setSerializerCompiler(serializerCompiler);
+app.setValidatorCompiler(validatorCompiler);
+
+app.register(fastifyCors);
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: "NLW Connect - NodeJS (Fev-2025)",
+      version: "0.0.1",
+    },
+  },
+  transform: jsonSchemaTransform,
+});
+
+app.register(fastifySwaggerUi, {
+  routePrefix: "/docs",
+});
+
+// . Importação de rotas
+app.register(subscribeToEventRoute);
+
+// . Inicialização do servidor
+app.listen({ port: env.PORT }).then(() => {
+  console.log("Server is running on port 3333");
+});
